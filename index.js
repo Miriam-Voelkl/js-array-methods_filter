8aba8608bc754a6719f1186ce49efa7496eddaab
Miriam Völkl <miriamvoelkl@gmail.com>
2023-12-15 12:51:52 +0100

    add solutions

--- index.js
+++ index.js
@@ -23,13 +23,21 @@ const cards = [
   },
 ];
 
-const onlyCardWithIdTwo = null;
+const onlyCardWithIdTwo = cards.filter((card) => card.id == "2");
 
-const allCardsWith3Tags = null;
+const allCardsWith3Tags = cards.filter((card) => card.tags.length == "3");
 
-const allCardsThatAreNotBookmarked = null;
+const allCardsThatAreNotBookmarked = cards.filter((card) => {
+  return !card.isBookmarked;
+});
 
-const allCardsWithTagsHTMLOrJSThatAreBookmarked = null;
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards
+  .filter((card) => {
+    return card.tags.includes("html") || card.tags.includes("js");
+  })
+  .filter((card) => card.isBookmarked);
+
+console.log(allCardsWithTagsHTMLOrJSThatAreBookmarked);
 
 export {
   onlyCardWithIdTwo,
